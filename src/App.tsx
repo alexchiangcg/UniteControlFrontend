228aa0bb226276060704daf53cf4cf475ddde549
alexChiang <alexchiang.cg@gmail.com>
2024-12-02 14:35:40 +0800

    chore: add antd and day.js 還有範例實現

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  Button,
+  Space,
+  DatePicker,
+  version,
+  ConfigProvider,
+  message,
+} from "antd";
+import zhTW from "antd/locale/zh_TW";
+import 'dayjs/locale/zh-tw';
+import dayjs from 'dayjs';
+dayjs.locale('zh-tw');
+
+const TestAntd = () => {
+  const [date, setDate] = useState(null);
+  const [messageApi, contextHolder] = message.useMessage();
+  const handleChange = (value) => {
+    console.log('handleChange.value:', value);
+    
+    messageApi.info(
+      `選擇的日期是: ${value ? value.format("YYYY年MM月DD日") : "未選擇"}`
+    );
+    setDate(value);
+  };
+  return (
+    <ConfigProvider locale={zhTW}>
+      <div style={{ width: 400, margin: "100px auto" }}>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>當前日期：{date ? date.format('YYYY年MM月DD日'): "未選擇"}</div>
+      </div>
+      {contextHolder}
+    </ConfigProvider>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,10 +62,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className='text-9xl text-red-700 font-black'>test tailwind</p>
+      <p className="text-9xl text-red-700 font-black">test tailwind</p>
 
+      <hr />
+      <h1>antd version: {version}</h1>
+      <Space>
+        <DatePicker />
+        <Button type="primary" onClick={()=>{
+            message.info("click primary Button with message");
+        }}>Primary Button</Button>
+      </Space>
+      <TestAntd></TestAntd>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
