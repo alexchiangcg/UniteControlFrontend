565cbac669b2cae5be2f5d8c9acb12536d5a0aa3
alexChiang <alexchiang.cg@gmail.com>
2024-12-23 20:44:04 +0800

    feat: user slice 全域可以讀取 user list

---
+++ src/slices/userSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from '../services/userListServices';
+
+interface UserState {
+    users: User[];
+}
+
+const initialState: UserState = {
+    users: [],
+};
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUsers: (state, action: PayloadAction<User[]>) => {
+            state.users = action.payload;
+        },
+    },
+});
+
+export const { setUsers } = userSlice.actions;
+export default userSlice.reducer;
