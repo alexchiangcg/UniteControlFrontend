8d647ff8fa8c641d330ad3f4e968cd26cfd608e2
alexChiang <alexchiang.cg@gmail.com>
2025-02-25 16:56:25 +0800

    chore: del nouse

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Input, Button, message } from "antd";
 import Header from "../../components/Header";
