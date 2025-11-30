47669c6ebaa22e9f6971b7298470793bd4570f25
alexChiang <alexchiang.cg@gmail.com>
2025-11-30 15:57:48 +0800

    feat: 新增 userManagementApi 至 Redux store 配置

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { registerApi, loginApi, logoutApi } from '@features/auth';
 import userListApi from '@shared/services/userListServices';
 import userReducer from '@shared/slices/userSlice';
+import { userManagementApi } from '@features/maintainer-manager/services/userManagementServices';
 
 export const store = configureStore({
     reducer: {
@@ -9,14 +10,16 @@ export const store = configureStore({
         [userListApi.reducerPath]: userListApi.reducer,
         [loginApi.reducerPath]: loginApi.reducer,
         [logoutApi.reducerPath]: logoutApi.reducer,
+        [userManagementApi.reducerPath]: userManagementApi.reducer,
         userReducer: userReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
-            registerApi.middleware, 
-            userListApi.middleware, 
-            loginApi.middleware, 
-            logoutApi.middleware),
+            registerApi.middleware,
+            userListApi.middleware,
+            loginApi.middleware,
+            logoutApi.middleware,
+            userManagementApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
